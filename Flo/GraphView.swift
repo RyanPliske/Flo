1477f3a859ff35e10cb46023714113af4732e2c7
@tPlisk <ryanpliske@yahoo.com>
2015-10-17 14:00:26 -0500

    Clipped the corners

--- Flo/GraphView.swift
+++ Flo/GraphView.swift
@@ -6,6 +6,13 @@ import UIKit
     @IBInspectable var endColor: UIColor = UIColor.greenColor()
     
     override func drawRect(rect: CGRect) {
+//        let width = rect.width
+//        let height = rect.height
+        
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: UIRectCorner.AllCorners, cornerRadii: CGSize(width: 8.0, height: 8.0))
+        path.addClip()
+        
+        
         let cgContext = UIGraphicsGetCurrentContext()
         let colors = [startColor.CGColor, endColor.CGColor]
         let colorSpace = CGColorSpaceCreateDeviceRGB()
