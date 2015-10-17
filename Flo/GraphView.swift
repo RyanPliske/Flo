d445c8e11cb1d7394db344a6ee5068a3eba4e83a
@tPlisk <ryanpliske@yahoo.com>
2015-10-17 13:47:28 -0500

    Drew a Gradient

--- Flo/GraphView.swift
+++ Flo/GraphView.swift
@@ -1,5 +1,19 @@
 import UIKit
 
-class GraphView: UIView {
+@IBDesignable class GraphView: UIView {
     
+    @IBInspectable var startColor: UIColor = UIColor.redColor()
+    @IBInspectable var endColor: UIColor = UIColor.greenColor()
+    
+    override func drawRect(rect: CGRect) {
+        let cgContext = UIGraphicsGetCurrentContext()
+        let colors = [startColor.CGColor, endColor.CGColor]
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let colorLocations:[CGFloat] = [0.0, 1.0]
+        let cgGradient = CGGradientCreateWithColors(colorSpace, colors, colorLocations)
+        let startPoint = CGPoint.zero
+        let endPoint = CGPoint(x: 0, y: CGRectGetHeight(bounds))
+        //TODO: Check out CGGradientDrawingOptions
+        CGContextDrawLinearGradient(cgContext, cgGradient, startPoint, endPoint, CGGradientDrawingOptions.DrawsBeforeStartLocation)
+    }
 }
